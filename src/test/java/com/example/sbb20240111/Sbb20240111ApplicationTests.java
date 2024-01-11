4795c33a948a95da47212b7ee044e45a81cac8cb
J-Cheol <lunaris0206@naver.com>
2024-01-11 13:37:16 +0900

    start

---
+++ src/test/java/com/example/sbb20240111/Sbb20240111ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.sbb20240111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb20240111ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
